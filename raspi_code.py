c90116a88ef668dbe12405dacb4a7bdd83c0aa78
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-02-15 13:07:53 -0500

    Improved cleanup.

--- raspi_code.py
+++ raspi_code.py
@@ -7,6 +7,7 @@ cap = cv2.VideoCapture(0)
 def detectFaces(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    print faces
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
@@ -30,3 +31,4 @@ while(True):
 
 def cleanup():
     cv2.destroyAllWindows()
+    cap.release()
