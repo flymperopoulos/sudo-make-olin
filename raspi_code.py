596f2c2d9eae879568293cc88d0af5eb795ed5bb
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-02-15 14:05:13 -0500

    Added more classifiers to classify more faces. Keepin' it classy.

--- raspi_code.py
+++ raspi_code.py
@@ -2,18 +2,26 @@ import cv2
 import numpy as np
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_profile_cascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
+print face_cascade.empty()
 cap = cv2.VideoCapture(0)
 
 def detectFaces(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     print faces
+    morefaces = face_profile_cascade.detectMultiScale(gray, 1.3, 5)
+    np.append(faces,morefaces)
+    print faces
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        print roi_color
     return frame
+
+def cleanup():
+    cv2.destroyAllWindows()
+    cap.release()
 
 while(True):
     # Capture frame-by-frame
@@ -28,7 +36,3 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cleanup()
         break
-
-def cleanup():
-    cv2.destroyAllWindows()
-    cap.release()
