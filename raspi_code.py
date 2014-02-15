8468790e0099a853ad79fae475228fd233f6a01e
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-02-15 12:31:45 -0500

    Added code for the raspi, to detect faces and send them to the internet. Still very much WIP.

---
+++ raspi_code.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+def detectFaces(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        print roi_color
+    return frame
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    result = detectFaces(frame)
+    #result = frame
+
+    # Display the resulting frame
+    cv2.imshow('frame',result)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cleanup()
+        break
+
+def cleanup():
+    cv2.destroyAllWindows()
