100cba1c0586a11eb0f14439de625ce6e3948065
Dakota Nelson <dakota.nelson@students.olin.edu>
2014-02-15 16:46:02 -0500

    Restructuring, and some work on the server.

--- server_code.py
+++ server_code.py
@@ -2,11 +2,13 @@ import requests
 import simplejson as json
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='')
 
 @app.route("/")
 def hello():
-    return 'Hello world!'
+    #return send_from_directory('','index.html')
+    #return app.send_static_file('index.html')
+    return render_template('index.html',None)
 
 @app.route("/test")
 def test():
